67d8dcf9623d2a77debfbd6cc0a5113fb8fa3f77
SD <harrypotter.starwars.shrish@gmail.com>
2020-11-18 19:20:39 +0530

    Revert "Switch to c test"
    This reverts commit 222e846ca34613cc564d9f7f82e1ff43650c7ecc.

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,9 @@
 #include "../src/hexjson.h"
 
+using namespace std;
+
 int main() {
     json_str jsonStr = str_value_of("hello");
-    printf("%s%s", jsonStr.str_value().c_str(), "\n");
+    cout << jsonStr.str_value() << endl;
     return 0;
 }
