826d237d27fc5787b6e3a49015f138a517d36e8e
SD <harrypotter.starwars.shrish@gmail.com>
2020-11-18 13:25:42 +0530

    Remove extra includes
     Changes to be committed:
    	modified:   src/hexjson.h

--- src/hexjson.h
+++ src/hexjson.h
@@ -1,11 +1,6 @@
 #ifndef HEXJSON_HEXJSON_H
 #define HEXJSON_HEXJSON_H
 
-#include "json_str.cpp"
-#include "json_bool.cpp"
-#include "json_number.cpp"
-#include "json_null.cpp"
-#include "json_object.cpp"
 #include "sutils.cpp"
 #include "deserializer.cpp"
 #include <string>
